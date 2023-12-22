791d1917a339e6fcf42335f53a5072e58cfaeec0
Jun Seong Pyo <standardstar@m365.hanyang.ac.kr>
2023-12-22 18:56:55 +0900

    add cookie, change order

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,43 @@
-import RegisterForm from "@/containers/register/register-form";
+"use client";
+import { Button } from "@/components/ui/button";
 import { Flex, Text } from "@radix-ui/themes";
+import {
+  AUTH_PROVIDER,
+  SUPPORTED_ETHEREUM_NETWORKS,
+  SignInResult,
+  THEME,
+  WALLET_PROVIDER,
+  init,
+  signIn,
+} from "@ramper/ethereum";
+import Cookies from "js-cookie";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 export default function Home() {
+  const [result, setResult] = useState<SignInResult>();
+  async function initWallet() {
+    init({
+      appName: "포로로",
+      authProviders: [
+        AUTH_PROVIDER.GOOGLE,
+        AUTH_PROVIDER.FACEBOOK,
+        AUTH_PROVIDER.TWITTER,
+        AUTH_PROVIDER.APPLE,
+        AUTH_PROVIDER.EMAIL,
+      ],
+      walletProviders: [WALLET_PROVIDER.METAMASK],
+      network: SUPPORTED_ETHEREUM_NETWORKS.MATICMUM,
+      theme: THEME.DARK,
+    });
+    const signInResult: SignInResult = await signIn();
+    setResult(signInResult);
+    if (result?.user) {
+      Cookies.set("userId", result.user.UID);
+      Cookies.set("publicKey", result.user.wallets.ethereum.publicKey);
+    }
+  }
+
   return (
     <main className="h-full w-full">
       <Flex
@@ -11,14 +48,33 @@ export default function Home() {
         <div className="bg-white p-0 w-7/12 shadow-sm rounded-md border-2 border-gray-200 overflow-hidden max-md:w-full">
           <div className="bg-slate-950 flex flex-col align-middle justify-start px-6 py-5 border-b-2 border-gray-200">
             <Text size="6" weight="bold" className="text-gray-50">
-              회원가입
-            </Text>
-            <Text size="2" weight="medium" className="text-gray-400">
-              운영진 확인 후 가입이 승인됩니다.
+              LOGIN WITH RAMPER
             </Text>
           </div>
-          <div className="flex flex-col justify-start p-6 md:w-8/12">
-            <RegisterForm />
+          <div className="flex flex-col justify-between p-6 md:w-8/12 h-96">
+            <div className="flex flex-col gap-5">
+              <Image
+                src="/ramper_black.png"
+                width={628}
+                height={162}
+                alt="램퍼 로고"
+              />
+              <p>
+                Ramper는 복잡한 단계와 Private Key를 필요로 하지 않고 암호화폐
+                지갑에 접근할 수 있도록 합니다.
+              </p>
+            </div>
+            <div>
+              {result ? (
+                <Button type="button">
+                  <Link href={"/auth/signup"}>다음</Link>
+                </Button>
+              ) : (
+                <Button type="button" onClick={initWallet}>
+                  로그인
+                </Button>
+              )}
+            </div>
           </div>
         </div>
       </Flex>
--- src/containers/register/register-form.tsx
+++ src/containers/register/register-form.tsx
@@ -1,26 +1,35 @@
-"use client"
+"use client";
 
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage, useFormField } from "@/components/ui/form"
-import z from 'zod';
-import { Input } from "@/components/ui/input"
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@/components/ui/button";
-import Space from "@/components/ui/space";
-
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import z from "zod";
 const formSchema = z.object({
-  username: z.string().min(2, { message: '이름은 2글자 이상이어야 합니다.' }).max(20, { message: '이름은 20글자 이하여야 합니다.' }),
-  studentId: z.string().length(12, { message: '학번은 12글자여야 합니다.' }),
+  username: z
+    .string()
+    .min(2, { message: "이름은 2글자 이상이어야 합니다." })
+    .max(20, { message: "이름은 20글자 이하여야 합니다." }),
+  studentId: z.string().length(12, { message: "학번은 12글자여야 합니다." }),
   profileImage: z.string(),
-})
+});
 
 export default function RegisterForm() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: '',
-      studentId: '',
-      profileImage: '',
+      username: "",
+      studentId: "",
+      profileImage: "",
     },
   });
 
@@ -32,56 +41,50 @@ export default function RegisterForm() {
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
         <FormField
-          name='username'
+          name="username"
           control={form.control}
           render={({ field }) => (
             <FormItem>
               <FormLabel>이름</FormLabel>
               <FormDescription>이름을 입력해주세요.</FormDescription>
               <FormControl>
-                <Input placeholder='이름'
-                  {...field}
-                />
+                <Input placeholder="이름" {...field} />
               </FormControl>
               <FormMessage />
             </FormItem>
           )}
         />
         <FormField
-          name='studentId'
+          name="studentId"
           control={form.control}
           render={({ field }) => (
             <FormItem>
               <FormLabel>학번</FormLabel>
               <FormDescription>학번을 입력해주세요.</FormDescription>
               <FormControl>
-                <Input placeholder='학번'
-                  {...field}
-                />
+                <Input placeholder="학번" {...field} />
               </FormControl>
               <FormMessage />
             </FormItem>
           )}
         />
         <FormField
-          name='profileImage'
+          name="profileImage"
           control={form.control}
           render={({ field }) => (
             <FormItem>
               <FormLabel>프로필 이미지</FormLabel>
               <FormDescription>jpg, png 이미지만 가능합니다</FormDescription>
               <FormControl>
-                <Input type='file'
-                  {...field}
-                />
+                <Input type="file" {...field} />
               </FormControl>
               <FormMessage />
             </FormItem>
           )}
         />
-        <div className='h-8' />
-        <Button type='submit'>회원가입</Button>
+        <div className="h-8" />
+        <Button type="submit">회원가입</Button>
       </form>
     </Form>
-  )
+  );
 }
