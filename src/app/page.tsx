62abc517d02c835ac2d9ee9af364d664c7700ecf
Jun Seong Pyo <standardstar@m365.hanyang.ac.kr>
2023-12-31 17:31:37 +0900

    필요없는 사진 삭제 및 문구 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -76,25 +76,28 @@ export default function Home() {
           <div className="flex flex-col p-6 md:w-8/12 h-96">
             <div className="flex flex-col gap-5">
               <h1 className="text-3xl font-bold">FORIF</h1>
-              <p className="text-base break-keep">
-                포리프는 전공과 상관없이 프로그래밍을 배울 수 있게 하자는 신념을
-                바탕으로 2015년에 창설되었습니다.
-                <br />
-                <br />
-                전공, 학년, 나이, 성별, 그 어떤 것도 전혀 상관 없습니다.
-                포리프와 함께 프로그래밍을 향한 열정을 불태울 수 있다면
-                충분합니다 🔥
-                <br />
-                <br />
-                포리프에 대해 더 알고 싶으시다면,{" "}
-                <Link
-                  href={"https://bit.ly/forif2023"}
-                  target="_blank"
-                  className="text-gray-600 font-bold"
-                >
-                  여기를 클릭해주세요!
-                </Link>
-              </p>
+              <div className="text-base break-keep flex flex-col gap-2">
+                <p>
+                  포리프는 전공과 상관없이 프로그래밍을 배울 수 있게 하자는
+                  신념을 바탕으로 2015년에 창설되었습니다.
+                </p>
+
+                <p>
+                  전공, 학년, 나이, 성별, 그 어떤 것도 전혀 상관 없습니다.
+                  포리프와 함께 프로그래밍을 향한 열정을 불태울 수 있다면
+                  충분합니다 🔥
+                </p>
+                <p>
+                  포리프에 대해 더 알고 싶으시다면,{" "}
+                  <Link
+                    href={"https://bit.ly/forif2023"}
+                    target="_blank"
+                    className="text-gray-600 font-bold"
+                  >
+                    여기를 클릭해주세요!
+                  </Link>
+                </p>
+              </div>
             </div>
           </div>
           <div className="flex flex-row p-6 w-full justify-between items-center">
@@ -107,7 +110,7 @@ export default function Home() {
                 RAMPER 회원가입
               </Button>
             )}
-            <div className="flex flex-row gap-3 justify-start items-center">
+            <div className="flex-row gap-3 justify-start items-center md:flex hidden">
               <Link
                 href={"https://www.instagram.com/forif_hyu/"}
                 target="_blank"
